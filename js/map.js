ff89e9f387fdb976b911a86799e5250f59176bed
Alex Troupe <adtroupe@uw.edu>
2015-10-18 14:16:01 -0700

    sunday commit

--- js/map.js
+++ js/map.js
@@ -1,21 +1,17 @@
 
+//This is the map variable and the how it is created in the JS.
+//Upon adding the map to the page, it calls the getData() function.
 var map;
-// Function to draw your map
 var drawMap = function() {
-// Create map and set view
-// Create a tile layer variable using the appropriate url
-// Add the layer to your map
-// Execute your function to get data
 	map = L.map('container').setView([39.50, -98.35], 4);
 	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 	layer.addTo(map);
 	getData();
 }
 
-// Function for getting data
+//This function gets the data out of the .json file via an Ajax request
+//Upon the retreival success, it calls the customBuild(data) function
 var getData = function() {
-// Execute an AJAX request to get the data in data/response.js
-// When your request is successful, call your customBuild function
 	var data;
 	$.ajax({
 		url: 'data/response.json',
@@ -28,23 +24,23 @@ var getData = function() {
 	})
 }
 
-// Loop through your data and add the appropriate layers and points
+//This function goes through the retreived data and manipulates it, building the cross-tabulation and the map layers
 var customBuild = function(data) {
-// Be sure to add each layer to the map
+	//These layers are layers of points that correspond to the different suspect races in the data
 	var unknown = new L.LayerGroup([]);
 	var white = new L.LayerGroup([]);
 	var black = new L.LayerGroup([]);
 	var asian = new L.LayerGroup([]);
 	var amIndian = new L.LayerGroup([]);
 	var island = new L.LayerGroup([]);
-
+	//This variable is for tracking each category in the cross-tabulation
 	var tableInfo = {
 		MenArmed: 0,
 		MenUnarmed: 0,
 		WomenArmed: 0,
 		WomenUnarmed: 0
 	};
-
+	//This loop parses through each police shooting entry
 	for (var i = 0; i < data.length; i++) {
 		var race = data[i].Race;
 		var lat = data[i].lat;
@@ -53,12 +49,11 @@ var customBuild = function(data) {
 		var outcome = data[i]['Hit or Killed?'];
 		var armed = data[i]['Armed or Unarmed?'];
 		var gender = data[i]["Victim's Gender"];
-
-		//WORKS HELLA YO
+		//This if else statement create the individual data points
 		var circle;
 		if (outcome == 'Killed') {
 			circle = new L.circleMarker([lat, lng], {
-				radius: 7,
+				radius: 8,
 				fillColor: "red",
 				fillOpacity: 0.3,
 				color: "darkred",
@@ -66,14 +61,14 @@ var customBuild = function(data) {
 			});
 		} else {
 			circle = new L.circleMarker([lat, lng], {
-				radius: 7,
+				radius: 8,
 				fillColor: "darkgray",
 				fillOpacity: 0.3,
 				color: "black",
 				opacity: 0.5
 			});
 		} 
-
+		//This if else statement determines which category the data falls under for the cross-tabulation and increments the number
 		if (gender == 'Male' && armed == 'Armed') {
 			tableInfo.MenArmed++;
 		} else if (gender == 'Male' && armed == 'Unarmed') {
@@ -84,19 +79,9 @@ var customBuild = function(data) {
 			tableInfo.WomenUnarmed++;
 		}
 
-		// MAKES MORE INTUITIVE SENSE, BUT NOT WORKING
-		// var circle = new L.circleMarker([lat, lng], {
-		// 	radius: 7,
-		// 	fillOpacity: 0.3,
-		// 	opacity: 0.5,
-		// 	className: outcome
-		// });
-		// $(this).addClass(outcome);
-		// $('.Killed').setStyle('fillColor', 'red').setStyle('color', 'darkred');
-		// $('.Hit').setStyle('fillColor', 'darkgray').setStyle('color', 'black');
-
 		circle.bindPopup(txt);
-
+		
+		//This adds the circle to the corresponding layer
 		if(race == 'White') {
 			circle.addTo(white);
 		} else if(race == 'Black or African American') {
@@ -111,7 +96,8 @@ var customBuild = function(data) {
 			circle.addTo(unknown);
 		}
 	}
-	
+
+	//This creates and populates the cross-tabulation
 	$('#table').append('<table class="table"></table>');
 	var row1 = $('<tr class="container"><td></td><td class="heading">Armed</td><td class="heading">Unarmed</td></tr>');
 	$('#table table').append(row1);
@@ -120,6 +106,7 @@ var customBuild = function(data) {
 	var row3 = $('<tr class="container"><td class="heading">Women</td><td>'+tableInfo.WomenArmed+'</td><td>'+tableInfo.WomenUnarmed+'</td></tr>');
 	$('#table table').append(row3);
 
+	//These .addTo functions add the individual race layers to the overall map
 	unknown.addTo(map);
 	white.addTo(map);
 	black.addTo(map);
@@ -127,7 +114,7 @@ var customBuild = function(data) {
 	amIndian.addTo(map);
 	island.addTo(map);
 
-// Once layers are on the map, add a leaflet controller that shows/hides layers
+	//The following is responsible for creating and adding a leaflet controller that shows/hides layers
 	var overlays = {
 		"White": white,
 		"Black or African American": black,
@@ -136,9 +123,7 @@ var customBuild = function(data) {
 		"Native Hawaiian or Pacific Islander": island,
 		"Unknown": unknown
 	}
-
 	L.control.layers(null, overlays).addTo(map);
-
 }
 
 
